206a60f4729924db89ab19120dbdd595552a75e9
Christos Sakaridis <christos.sakaridis@yahoo.com>
2015-04-11 12:35:02 +0200

    Modified input script for smartphone data to save focal length too in .mat files

--- MATLAB/Input_smartphone_video_data.m
+++ MATLAB/Input_smartphone_video_data.m
@@ -5,13 +5,13 @@ clear all;
 
 %% Read sensor data
 
-file_sensor_data = 'data/VID_20150407_230812.txt';
+file_sensor_data = 'data/VID_20150409_162957.txt';
 [F, time_sys_videostart, time_sys_sensor, time_ev_sensor, rot_mats] =...
     get_sensor_data(file_sensor_data);
 
 %% Read video from file and get basic properties
 
-file_video = 'data/VID_20150407_230812.mp4';
+file_video = 'data/VID_20150409_162957.mp4';
 vid = VideoReader(file_video);
 
 % Duration in ms
@@ -35,3 +35,5 @@ time_sensor_video = time_sys_sensor - time_sys_videostart;
 % Get cell array with single-frame sequences of sensor samples per cell
 samples_per_frame = video_sensor_registration(time_sensor_video, rot_mats, duration, num_frames);
 
+% Save frame-separated sensor data
+save(strcat(file_video(1:end-4), '_registered', '.mat'), 'samples_per_frame', 'F');
